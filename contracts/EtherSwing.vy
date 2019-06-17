061cd6a5b61691d0b4abfdf88e48fbcec846acbe
Sam Richards <sbrichards@gmail.com>
2019-06-17 14:35:12 -0700

    Add fund() & transfer()

--- contracts/EtherSwing.vy
+++ contracts/EtherSwing.vy
@@ -5,6 +5,22 @@ def __init__():
   self.owner = msg.sender
 
 @public
+@payable
+def fund():
+  assert msg.value > 0, "Must send value to call this function."
+
+@public
+def transfer(_recipient: address, _amount: uint256(wei)):
+  assert msg.sender == self.owner, "Must be contract owner to call this function."
+  assert self.balance >= _amount, "Insufficient contract balance."
+  send(_recipient, _amount)
+
+@public
 @constant
 def getOwner() -> address:
   return self.owner
+
+@public
+@constant
+def getBalance() -> uint256(wei):
+  return self.balance
--- test/etherSwing.js
+++ test/etherSwing.js
@@ -1,18 +1,62 @@
 const EtherSwing = artifacts.require('EtherSwing');
+const { expectRevert } = require('openzeppelin-test-helpers');
+const { expect } = require('chai');
 
 contract('EtherSwing', accounts => {
   let etherSwing;
   const owner = accounts[0];
+  const other = accounts[1];
 
   beforeEach(async () => {
     etherSwing = await EtherSwing.deployed();
   });
 
-  it('should have owner.', async () => {
-    assert.equal(
-      await etherSwing.getOwner(),
-      owner,
-      'contract deployer should be owner.'
-    );
+  describe('initial state', async () => {
+    it('should have owner.', async () => {
+      expect(await etherSwing.getOwner()).equal(owner);
+    });
+
+    it('should have no balance', async () => {
+      expect(await etherSwing.getBalance()).to.be.bignumber.equal('0');
+    });
+  });
+
+  describe('fund()', async () => {
+    it('should accept funds', async () => {
+      expect(await etherSwing.getBalance()).to.be.bignumber.equal('0');
+      await etherSwing.fund({ from: owner, value: 500 });
+      expect(await etherSwing.getBalance()).to.be.bignumber.equal('500');
+    });
+
+    it('should fail if no value is sent', async () => {
+      await expectRevert(
+        etherSwing.fund({ from: owner }),
+        'Must send value to call this function.'
+      );
+    });
+  });
+
+  describe('transfer()', async () => {
+    // TODO reset contract between tests?
+    it('should transfer valid funds', async () => {
+      expect(await etherSwing.getBalance()).to.be.bignumber.equal('500');
+      await etherSwing.transfer(owner, 300, { from: owner });
+      expect(await etherSwing.getBalance()).to.be.bignumber.equal('200');
+    });
+
+    it('should fail if not called by owner', async () => {
+      await expectRevert(
+        etherSwing.transfer(other, 300, { from: other }),
+        'Must be contract owner to call this function.'
+      );
+    });
+
+    it('should fail for invalid funds', async () => {
+      expect(await etherSwing.getBalance()).to.be.bignumber.equal('200');
+      await expectRevert(
+        etherSwing.transfer(owner, 800, { from: owner }),
+        'Insufficient contract balance.'
+      );
+    });
   });
 });
